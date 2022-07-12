b7921d2d5a9fe7e5eea6b1a538b4b1da4060683c
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-07-11 23:27:43 -0300

    feat: create extension

---
+++ README.md
@@ -0,0 +1,72 @@
+# Generate component and style
+
+This extension generates a component and style for the given component name.
+
+## About
+
+This extension the component with the given name, generates a test file with the [testing-library react](https://testing-library.com/react) library and the style that we can choose from 3 possibilities:
+- [Material-UI](https://material-ui.com/)
+- [Styled-Components](https://styled-components.com/)
+- Style without library
+- Without style
+
+## Usage
+
+### Step 1:
+- After the extension is installed, press the `Ctrl+Shift+P` or `Cmd+Shift+P` key combination to open the command palette.
+- Type `generate component` and press `Enter` to generate a component.
+
+<p align="left">
+  <img src="./src/img/step-one.gif" width="500" />
+</p>
+
+### Step 2:
+- Type the component name and press `Enter`.
+
+<p align="left">
+  <img src="./src/img/step-two.gif" width="500" />
+</p>
+
+### Step 3:
+- Choose the folder where you want to generate the component or choose path default (`src/components`).
+
+<p align="left">
+  <img src="./src/img/step-three.png" width="500" />
+</p>
+  
+### Step 4:
+- Choose the style library you want to use.
+- Choose between `Material-UI` or `Styled-Components` or `Style without library` or `Without style`.
+
+
+<p align="left">
+  <img src="./src/img/step-four.png" width="500" />
+</p>
+
+### Result:
+- The component is generated in the folder you choose with files:
+  - `component-style.ts`
+  - `component.spec.tsx`
+  - `component.tsx`
+  - `index.ts`
+    - Ps. All the files that are generated, contain the start code in each one.
+
+<p align="left">
+  <img src="./src/img/result.png" width="200" />
+</p>
+
+## Requirements
+
+If you want to use the style library `Material-UI`, you need to install the `@material-ui/core` package.
+```bash
+npm install @material-ui/core
+```
+If you want to use the style library `Styled-Components`, you need to install the `styled-components` package.
+```bash
+npm install styled-components
+```
+If you want to use the test library `testing-library react`, you need to install the `@testing-library/react` package.
+```bash
+npm install @testing-library/react
+```
+
---
+++ src/extension.ts
@@ -0,0 +1,166 @@
+import * as vscode from 'vscode';
+export function activate(context: vscode.ExtensionContext) {
+
+	function folderSelected(){
+		const getFolder = vscode.workspace.workspaceFolders;
+		if(getFolder){
+			const lastFolder = getFolder[getFolder.length - 1];
+			return lastFolder.uri.fsPath;
+		}
+		return "";
+	}
+
+	let disposable = vscode.commands.registerCommand('generate-component-and-style.gc', async () => {
+
+		async function getNameComponent(){
+			const name = await vscode.window.showInputBox({
+				placeHolder: "Component name",
+				value: "",
+				validateInput: (value: string) => {
+					if(value.length === 0){
+						return "Component name is required";
+					}
+					return null;
+				}
+			});
+			return name ? name : "";
+		}
+
+		async function getSelectedPath(){
+			const fs = require('fs');
+			const path = require('path');
+
+			const folderRoot = folderSelected();
+			const folderComponent = path.join(folderRoot, "src/components");
+			fs.mkdirSync(folderComponent, {recursive: true});
+
+			const folder = await vscode.window.showQuickPick([
+				{
+					label: "Default folder",
+					description: folderSelected() + "/src/components/",
+					detail: "",
+					picked: true
+				},
+				{
+					label: "Choose folder",
+					description: "",
+					detail: "",
+					picked: false
+				}
+			], {
+				placeHolder: "Choose folder"
+			}) as vscode.QuickPickItem;
+			if(folder.label === "Default folder"){
+				return folderSelected() + "/src/components/";
+			}
+			const folderPath = await vscode.window.showOpenDialog({
+				canSelectFiles: false,
+				canSelectFolders: true,
+				canSelectMany: false,
+				openLabel: "Select folder"
+			});
+			return folderPath ? folderPath[0].fsPath : "";
+
+		}
+
+		async function chooseLibrary(){
+			const library = await vscode.window.showQuickPick([
+				{
+					label: "Material UI",
+					description: "",
+					detail: "",
+					picked: true,
+					value: "material-ui"
+				},
+				{
+					label: "Styled Components",
+					description: "",
+					detail: "",
+					picked: false,
+					value: "styled-components"
+				},
+				{
+					label: "Create style without library",
+					description: "",
+					detail: "",
+					picked: false,
+					value: "no-library"
+				},
+				{
+					label: "Don't create style",
+					description: "",
+					detail: "",
+					picked: false,
+					value: "no-style"
+				}
+
+			], {
+				placeHolder: "Choose library"
+			}) as any;
+			return library ? library.value : "";
+		}
+
+		async function createComponent(nameComponent: string, pathSelected: string, library: string){
+			const fs = require('fs');
+			const path = require('path');
+
+			const nameComponentLowerCase = nameComponent.toLowerCase();
+
+			const createFolder = path.join(pathSelected, nameComponentLowerCase);
+			fs.mkdirSync(createFolder);
+			const componentFile = path.join(createFolder, nameComponentLowerCase + '.tsx');
+			
+			const indexFile = path.join(createFolder, 'index.ts');
+
+			const nameFunctionComponent = nameComponent.charAt(0).toUpperCase() + nameComponent.slice(1) + "Component";
+
+			switch(library){
+				case "material-ui":
+					const styleFileMui = path.join(createFolder, nameComponentLowerCase + '-style.ts');
+					const styleFileContentMui = "import { makeStyles } from '@material-ui/core/styles';\n\nexport default makeStyles({\n	container: {}\n});\n";
+					fs.writeFileSync(styleFileMui, styleFileContentMui);
+					const componentFileContentMui = "import makeStyles from './" + nameComponentLowerCase + "-style';\n\nfunction " + nameFunctionComponent + "() {\n	const classes = makeStyles();\n	return (\n		<h1 className={classes.container}>Hello World</h1>\n	);\n}\n\nexport default " + nameFunctionComponent + ";";
+					fs.writeFileSync(componentFile, componentFileContentMui);
+					break;
+				case "styled-components":
+					const styleFileSc = path.join(createFolder, nameComponentLowerCase + '-style.ts');
+					const styleFileContentSc = "import styled from 'styled-components';\n\nexport const Container = styled.div``;\n";
+					fs.writeFileSync(styleFileSc, styleFileContentSc);
+					const componentFileContentSc = "import { Container } from './" + nameComponentLowerCase + "-style';\n\nfunction " + nameFunctionComponent + "() {\n	return (\n		<Container>Hello World</Container>\n	);\n}\n\nexport default " + nameFunctionComponent + ";";
+					fs.writeFileSync(componentFile, componentFileContentSc);
+					break;
+				case "no-library":
+					const styleFileNoLibrary = path.join(createFolder, nameComponentLowerCase + '-style.ts');
+					const styleFileContentNoLibrary = "export const container = {};\n";
+					fs.writeFileSync(styleFileNoLibrary, styleFileContentNoLibrary);
+					const componentFileContentNoLibrary = "import { container } from './" + nameComponentLowerCase + "-style';\n\nfunction " + nameFunctionComponent + "() {\n	return (\n		<h1 className={container}>Hello World</h1>\n	);\n}\n\nexport default " + nameFunctionComponent + ";";
+					fs.writeFileSync(componentFile, componentFileContentNoLibrary);
+					break;
+				case "no-style":
+					const componentFileContentNoStyle = "function " + nameFunctionComponent + "() {\n	return (\n		<h1>Hello World</h1>\n	);\n}\n\nexport default " + nameFunctionComponent + ";";
+					fs.writeFileSync(componentFile, componentFileContentNoStyle);
+					break;
+			}
+
+			const indexFileContent = `export { default as ${nameComponentLowerCase.charAt(0).toUpperCase() + nameComponentLowerCase.slice(1) + "Tag"} } from './${nameComponentLowerCase}';`;
+			fs.appendFileSync(indexFile, indexFileContent);
+
+			const testFileContent = `import { render } from '@testing-library/react';\nimport ${nameFunctionComponent} from './${nameComponentLowerCase}';\n\nconst makeSut = () => render(<${nameFunctionComponent} />);\n\ndescribe('${nameFunctionComponent}', () => {\n	test('should render', () => {\n		makeSut();\n	});\n});\n`;
+			const testFile = path.join(createFolder, nameComponentLowerCase + '.spec.tsx');
+			fs.writeFileSync(testFile, testFileContent);
+
+			vscode.window.showInformationMessage(`Component ${nameComponent} created successfully`);
+			
+		}
+
+		const nameComponent = await getNameComponent();
+		const pathSelected = await getSelectedPath();
+		const library = await chooseLibrary();
+		createComponent(nameComponent, pathSelected, library);
+		
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
